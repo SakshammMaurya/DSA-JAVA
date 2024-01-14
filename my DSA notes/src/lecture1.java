77355714dcb21b6418154a10231afd95f43321b5
SAKSHAM MAURYA <2022ugee079@nitjsr.ac.in>
2024-01-14 15:50:54 +0530

    checking how it works

---
+++ my DSA notes/src/lecture1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class lecture1 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int j= sc.nextInt();
+        while(j-->0){
+            int n= sc.nextInt();
+            int p=  sc.nextInt();
+            System.out.println(n*p);
+        }
+
+    }
+}
