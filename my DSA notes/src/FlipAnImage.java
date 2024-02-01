249d5707c32f9ad7072957e906442e3e0b5e4de7
SAKSHAM MAURYA <2022ugee079@nitjsr.ac.in>
2024-02-01 19:55:06 +0530

    array question

---
+++ my DSA notes/src/FlipAnImage.java
@@ -0,0 +1,28 @@
+//    https://leetcode.com/problems/flipping-an-image/description/
+public class FlipAnImage {
+    class Solution {
+        public int[][] flipAndInvertImage(int[][] image) {
+
+            for(int i=0;i<image.length;i++) reverse(image[i],image.length);
+            for(int i=0;i<image.length;i++){
+                for(int j=0;j<image.length;j++){
+                    if(image[i][j]==1) image[i][j]=0;
+                    else image[i][j]=1;
+
+                }
+            }
+            return image;
+        }
+
+        static int[] reverse(int[] arr,int n){
+            int i, k, t;
+            for (i = 0; i < n / 2; i++) {
+                t = arr[i];
+                arr[i] = arr[n - i - 1];
+                arr[n - i - 1] = t;
+            }
+            return arr;
+
+        }
+    }
+}
---
+++ my DSA notes/src/Lecture13_1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+//   https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+public class Lecture13_1 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+
+    }
+}
